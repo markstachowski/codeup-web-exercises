41e9738ec16a54b0a2a44763beed3f6df2f2b461
Mark Stachowski <markstachowski@me.com>
2018-10-08 19:08:20 -0500

    while loop exercise updated

---
+++ js/while.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var numberList = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536];
+var i = 0;
+
+while (i < numberList.length) {
+    console.log(numberList[i]);
+    i++;
+}
+
+
