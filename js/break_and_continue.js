8c79fb15c3ed0f1160ab292517254c0cdea608c4
Mark Stachowski <markstachowski@me.com>
2018-10-11 08:39:13 -0500

    added break_and_continue exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,7 @@
+"use strict";
+// Instructions for break_and_continue.js
+// 1. Create a file named break_and_continue.js in the js directory.
+// 2. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+// 3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+
