e9ccb849bfa680ce5490df6572f769910d1e13ac
Mark Stachowski <markstachowski@me.com>
2018-10-11 11:29:02 -0500

    updating break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,34 @@
+//function () {
 "use strict";
 // Instructions for break_and_continue.js
 // 1. Create a file named break_and_continue.js in the js directory.
 // 2. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
 // 3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+    var blogPost = {};
+    blogPost.author = "Mark";
+    blogPost.averageRating = 5;
+    blogPost.datePosted = "Sept. 25, 2000";
+    blogPost.title = "Best blog";
+    blogPost.noOfShares = 500;
+    blogPost.NoOfViews = 500;
+    blogPost.isFlagged = true;
+    blogPost.lastUpdated = "Oct. 31, 2001";
+    blogPost.content = "Biography";
 
+    console.log(blogPost);
 
+    var blogPost2 = {
+        author: "Mark",
+        averageRating: 5,
+        datePosted: "Dec. 30, 2000",
+        title: "Best blog",
+        noOfShares: 500,
+        NoOfViews: 500,
+        isFlagged: true,
+        lastUpdated: "Jan. 25, 2010",
+        content: "Biography"
+    };
+
+    console.log(blogPost2);
+
+//}();
