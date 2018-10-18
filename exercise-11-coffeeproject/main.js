f7be55bac9218646597353fe141f33ad93ff6ab1
Mark Stachowski <markstachowski@me.com>
2018-10-18 00:12:54 -0500

    working on coffee project

---
+++ exercise-11-coffeeproject/main.js
@@ -0,0 +1,58 @@
+"use strict"
+
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    html += '<h4>' + coffee.name + '</h4>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+
+    return html;
+}
+
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = 0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        } else if (coffee.all === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light', all: 'all'},
+    {id: 2, name: 'Half City', roast: 'light', all: 'all'},
+    {id: 3, name: 'Cinnamon', roast: 'light', all: 'all'},
+    {id: 4, name: 'City', roast: 'medium', all: 'all'},
+    {id: 5, name: 'American', roast: 'medium', all: 'all'},
+    {id: 6, name: 'Breakfast', roast: 'medium', all: 'all'},
+    {id: 7, name: 'High', roast: 'dark', all: 'all'},
+    {id: 8, name: 'Continental', roast: 'dark', all: 'all'},
+    {id: 9, name: 'New Orleans', roast: 'dark', all: 'all'},
+    {id: 10, name: 'European', roast: 'dark', all: 'all'},
+    {id: 11, name: 'Espresso', roast: 'dark', all: 'all'},
+    {id: 12, name: 'Viennese', roast: 'dark', all: 'all'},
+    {id: 13, name: 'Italian', roast: 'dark', all: 'all'},
+    {id: 14, name: 'French', roast: 'dark', all: 'all'},
+];
+
+var tbody = document.querySelector('#coffees');
+var submitButton = document.querySelector('#submit');
+var roastSelection = document.querySelector('#roast-selection');
+
+tbody.innerHTML = renderCoffees(coffees);
+
+submitButton.addEventListener('click', updateCoffees);
